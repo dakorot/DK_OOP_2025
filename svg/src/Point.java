0a33932cbd4e4d69794745eaef0cc4c70ca14c0a
dakorot <koropenko.daria@gmail.com>
2025-02-27 16:19:16 +0100

    Translating

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -5,4 +5,21 @@ public class Point {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
+
+    public String toSvg() {
+        return "<circle r=\"5\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"red\" />";
+    }
+
+    public void translate(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public Point translated(double dx, double dy) {
+        Point newPoint = new Point();
+        newPoint.x = this.x + dx;
+        newPoint.y = this.y + dy;
+
+        return newPoint;
+    }
 }
